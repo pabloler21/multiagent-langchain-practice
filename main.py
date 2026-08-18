7c2467aaa9aa71e994b0af863d14f7a41057b01d
pabloler21 <pabloo.ale1111@gmail.com>
2026-08-18 16:28:17 -0300

    add dependencies of dotenv and create config.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from multiagent-langchain-practice!")
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+
+import os
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()  # Load environment variables from .env file
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+chat = ChatOpenAI(
+    model_name="gpt-3.5-turbo",
+    openai_api_key=OPENAI_API_KEY,
+    temperature=0.7,
+    max_tokens=150,
+)
